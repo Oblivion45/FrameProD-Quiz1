3ba2017f834a5e3a52624b23388695ecff9023a3
Rizqi Rifaldi <81369211+Oblivion45@users.noreply.github.com>
2022-03-12 11:43:59 +0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="quiz1.Global" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace quiz1
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+
+            ///System.Web.Routing.RouteTable.Routes.MapPageRoute("Obliv Homepage", "q1", "~/Default.aspx");
+            ///System.Web.Routing.RouteTable.Routes.MapPageRoute("Obliv Website Profile", "q1/profile", "~/profile.aspx");
+            ///System.Web.Routing.RouteTable.Routes.MapPageRoute("Obliv Company Profile", "q1/company", "~/company.aspx");
+            ///System.Web.Routing.RouteTable.Routes.MapPageRoute("Obliv Furniture Catalog", "q1/product", "~/product.aspx");
+            ///System.Web.Routing.RouteTable.Routes.MapPageRoute("Obliv Collaboration", "q1/collaboration", "~/collaboration.aspx");
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
